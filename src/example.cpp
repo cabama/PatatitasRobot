4234d8c213c8f3502ce4f1f5a9ea522d799f09ab
cabama <barreymata@gmail.com>
2016-03-04 21:02:32 +0100

    add mainers created before.

---
+++ src/example.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(){
+	std::cout << "Introducir dos numeros: "<< std::endl;
+	int n1, n2;
+	std::cin >> n1 >> n2;
+	std::cout << "La suma de " << n1 << "+" << n2 << "es" << n1+n2 << std::endl;
+}
