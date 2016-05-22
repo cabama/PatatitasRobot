a494895dc2aad4f20028bb0291939701485f7acb
butakus <butakus@gmail.com>
2016-05-22 20:43:44 +0200

    calibracion brujula

--- src/publicador_brujula.cpp
+++ src/publicador_brujula.cpp
@@ -19,7 +19,14 @@ using namespace std;
 const string nodo_name = "nodo_brujula";    // Nombre del nodo.
 const string topic_name = "brujula_bruto";  // Nombre del topic donde se va a publicar la brujula.
 float tiempo_muestreo = 0.5;                // Ts | Tiempo de muestreo para la lectura de la brujula.
-CompassMsg brujula_struct;                  // Struct desarrollado en la libreria de la brujula para recibir los tres datos.
+
+// Parametros de calibracion de la brujula
+double bias[3] = {-112.017978, -39.199350, -97.169059};
+// Filas de la matriz
+double A_0[3] = {0.089225, -0.001845, 0.006807};
+double A_1[3] = {-0.001845, 0.090054, 0.002206};
+double A_2[3] = {0.006807, 0.002206, 0.092495};
+
 
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
@@ -27,9 +34,19 @@ CompassMsg brujula_struct;                  // Struct desarrollado en la libreri
 
 /** 
  * Prototipo de la funcion calibración.
- * A esta funcion se le pasará el header bruto de la cabecera de la brujula
+ * Recibe los valores x, y, z brutos de la brujula y los corrige
  */
-double calibracion(double& matriz);
+void calibracion(CompassMsg &mag){
+  mag.x -= bias[0];
+  mag.y -= bias[1];
+  mag.z -= bias[2];
+  double new_x = A_0[0]*mag.x + A_0[1]*mag.y + A_0[2]*mag.z;
+  double new_y = A_1[0]*mag.x + A_1[1]*mag.y + A_1[2]*mag.z;
+  double new_z = A_2[0]*mag.x + A_2[1]*mag.y + A_2[2]*mag.z;
+  mag.x = new_x;
+  mag.y = new_y;
+  mag.z = new_z;
+}
 
 
 /**
@@ -44,7 +61,7 @@ int main(int argc, char **argv)
   ROS_INFO("nodo_brujula creado y registrado");
   // Cargamos el modulo de la brujula.
   HMC5883L brujula;
-  CompassMsg mensaje_lib;
+  CompassMsg brujula_struct;
   int respuesta = brujula.conectamos_brujula();
   ROS_INFO("Modulo de la brujula cargado; (id-i2c = %d)", respuesta);
   // Definimos el publicador.
@@ -58,6 +75,7 @@ int main(int argc, char **argv)
 
     std_msgs::Float64 mensaje_ros;
     brujula_struct = brujula.get_data();
+    calibracion(brujula_struct);
     printf("Magnetometro [x, y, z] = [%d, %d, %d] - Angulo = %f \n", brujula_struct.x, brujula_struct.y, brujula_struct.z, brujula_struct.angle);
     ROS_INFO("mensaje");
     publicador.publish(mensaje_ros);
